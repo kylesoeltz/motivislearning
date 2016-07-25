a3f2197365ecf756791e6c0a06eeabaceafdcee1
kylesoeltz <kyle@digitalartisans.com>
2016-07-25 09:15:27 -0400

    Updates for WordPress syntax

--- index.php
+++ index.php
@@ -14,56 +14,48 @@
         <?php include 'includes/header.php'; ?>
         <main>
 	        <?php include 'includes/front-page-slider.php'; ?>
-        	<article class="full-width">
+        	<article>
 	        	<div class="inner">
-	        		<section>
+	        		<section class="text-center">
 		        		<div class="row">
-			        		<h2 class="text-center">The Next Generation Learning Platform<br>Built on Salesforce</h2>
+			        		<h2>The Next Generation Learning Platform<br>Built on Salesforce</h2>
 		        		</div>
 		        		<div class="row">
 			        		<div class="callouts">
 				        		<div class="col-md-8">
 					        		<i class="fa fa-medal title-icon"></i>
-					        		<h4 class="text-center">Learning Relationship Management</h4>
-					        		<p class="text-center">
+					        		<h4>Learning Relationship Management</h4>
+					        		<p>
 					        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.
 					        		</p>
-					        		<div class="center-btn">
-							            <a href="#" class="btn-solid-orange">Learn More</a>
-						            </div>
+						            <a href="#" class="btn-solid-orange">Learn More</a>
 				        		</div>
 				        		<div class="col-md-8">
 					        		<i class="fa fa-social title-icon"></i>
-					        		<h4 class="text-center">Social Networking &amp; School Community</h4>
-					        		<p class="text-center">
+					        		<h4>Social Networking &amp; School Community</h4>
+					        		<p>
 					        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.
 					        		</p>
-					        		<div class="center-btn">
-							            <a href="#" class="btn-solid-orange">Learn More</a>
-						            </div>
+						            <a href="#" class="btn-solid-orange">Learn More</a>
 				        		</div>
 				        		<div class="col-md-8">
 					        		<i class="fa fa-career-path title-icon"></i>
-					        		<h4 class="text-center">Career Pathing For Students &amp; Employers</h4>
-					        		<p class="text-center">
+					        		<h4>Career Pathing For Students &amp; Employers</h4>
+					        		<p>
 					        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.
 					        		</p>
-					        		<div class="center-btn">
-							            <a href="#" class="btn-solid-orange">Learn More</a>
-						            </div>
+						            <a href="#" class="btn-solid-orange">Learn More</a>
 				        		</div>
 			        		</div>
 		        		</div>
 		        	</section>
 	        	</div>
         	</article>
-        	<article class="full-width navy">
+        	<article class="full-width navy text-center">
 	        	<div class="inner">
 	        		<section>
-		        		<h4 class="open-space text-center">Learn About The Benefits Of Salesforce For Higher Education</h4>
-		        		<div class="center-btn">
-				        	<a href="#" class="btn-ghost-white">Learn More</a>
-			        	</div>
+		        		<h4>Learn About The Benefits Of Salesforce For Higher Education</h4>
+			        	<a href="#" class="btn-ghost-white">Learn More</a>
 	        		</section>
         		</div>
         	</article>
