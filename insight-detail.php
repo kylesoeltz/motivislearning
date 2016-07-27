4049d0ca5b0640b5f199fde8f10014ec438edd2c
Greg Saras <Greg@Stephanies-MacBook-Pro.local>
2016-07-27 12:39:30 -0400

    Insight Detail Page
    Uploading initial insight details page. Might need a few tweaks but
    should be mostly all set.

---
+++ insight-detail.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+
+        <title>Motivis</title>
+        <meta name="description" content="">
+        <?php include 'includes/head.php'; ?>
+
+    </head>
+    <body>
+        <?php include 'includes/header.php'; ?>
+        <main>
+        	<header class="page-header featured-header" style="background-image: url(images/featured-section.jpg);">
+	        	<section class="inner">
+		        	<address>Emily Dustin  |  June 10, 2016</address>
+	        		<h1>Cornell Brings Online Community Alive, Reduces Email Overload</h1>
+        		</section>
+        	</header>
+        	<article class="insight-text">
+        		<div class="inner">
+        			<section>
+        				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.</p>
+        		
+        				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.</p>
+        		
+        				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.</p>
+        		
+        				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum.</p>
+        		
+        				<blockquote>Quote from Paul about Saleforce and the future of education lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque velit tortor, aliquet a gravida vitae, venenatis vitae.</blockquote>
+			    		<cite>Dr. Paul LeBlanc <small>President/CEO SNHU</small></cite>
+        		
+        				<p>Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ligula malesuada, efficitur sem id, ultricies eros. Ut blandit nulla turpis, sit amet tempor massa iaculis nec. Praesent imperdiet lacus vel nunc fermentum egestas.</p>	
+        			</section>
+        			<section class="post-nav">
+        				<a href="#"><i class="fa fa-left-arrow" aria-hidden="true"></i>Previous Post</a>
+        				<a href="#">Next Post<i class="fa fa-right-arrow" aria-hidden="true"></i></a>
+        			</section>
+        			<section>
+        				<p>Tags:
+        					<span class="tag">Case Studies,</span>
+        					<span class="tag">Student Success</span>
+        					| Comments (0)
+        				</p>
+        			</section>
+        			<section class="standard-block about-author">
+		        		<img src="images/avatar-paul.jpg" class="avatar avatar-medium">
+		        		<div class="content">
+			        		<h4>about emily dustin</h4>
+			        		<p>Emily Dustin is a veteran New Hampshire educator with an M.Ed. in Educational Leadership. She joined Motivis Learning as an Educational Strategist to help inform product design and development and is a champion of competency education and student success. She is also kind of awesome! You can follow her on twitter @emilydustin #CBE #LRM #stdntcntrd</p>
+		        		</div>
+		        	</section>
+        			<section class="comments">
+        				<p><span>0 Comments</span></p>
+        				<input type="text" name="comment-input" placeholder="join the conversation..." />
+        			</section>
+        		</div>
+        	</article>
+        	<article class="navy text-center">
+	        	<div class="inner">
+	        		<section>
+		        		<h4>like what you're reading?</h4>
+			        	<a href="#" class="btn">subscribe for updates</a>
+	        		</section>
+        		</div>
+        	</article>
+        </main>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
