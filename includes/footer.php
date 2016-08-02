8e82f01be33ace1282b5e10e8e8531bb39006669
Greg Saras <Greg@Stephanies-MacBook-Pro.local>
2016-08-02 10:46:26 -0400

    New Footer
    Wrote the new footer, added required assets.

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,48 @@
 <footer>
-	<p class="social-icons">
-		<a href="#"><i class="fa fa-twitter"></i></a>
-		<a href="#"><i class="fa fa-facebook"></i></a>
-		<a href="#"><i class="fa fa-pinterest"></i></a>
-		<a href="#"><i class="fa fa-linkedin"></i></a>
-		<a href="#"><i class="fa fa-youtube"></i></a>
-		<a href="#"><i class="fa fa-rss"></i></a>
-		<a href="#"><i class="fa fa-google-plus"></i></a>
-	</p>
-	<small>© 2016 Motivis Learning. All Rights Reserved.</small>
+	<section>
+		<div class="row">
+			<div class="col-sm-12 footer-left">
+				<div class="left-content">
+					<h5>motivis learning</h5>
+					<section>
+						<h4><a href='#'>From the Field: Clearing the Path for CBE Within the UNC System</a></h4>
+						<small>6/20/2016 | Emily Dustin</small>
+					</section>
+					<section>
+						<h4><a href='#'>Artificial Roadblocks to Competency-Based Education</a></h4>
+						<small>6/20/2016 | AJ Dellicicchi</small>
+					</section>
+				</div>
+			</div>	
+			<div class="col-sm-12 footer-right">
+				<div class="right-content">
+					<h5>motivis platform</h5>
+					<ul>
+						<li><h4><a href="#">learning relationship management</a></h4></li>
+						<li><h4><a href="#">networking & school community</a></h4></li>
+						<li><h4><a href="#">student information systems</a></h4></li>
+						<li><a href="/demo.php" class="btn">Request Demo</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="image-dots">
+		<div class="inner">
+			<h1 class="logo"><a href="/">Motivis Learning</a></h1>
+				<div class="col-sm-16">
+						<p>Copyright 2016 Motivis Learning Systems<br />25 Pelham Road, Unit 204<br />Salem, NH 03079</p>
+						<p><tel>855-202-6500</tel><br />info@motivislearning.com</p>
+					</div>
+					<div class="col-sm-8">
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </footer>
 <script src="js/bootstrap.js"></script>
 <script src="js/plugins.js"></script>
