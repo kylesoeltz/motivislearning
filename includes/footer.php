7d06e10fe4b16709585fb8fa43ef0c7c1894c239
Brendan Sands <brendansands@gmail.com>
2016-08-04 22:53:16 -0400

    MOre footer updates

--- includes/footer.php
+++ includes/footer.php
@@ -4,25 +4,25 @@
 			<div class="col-sm-12 footer-left">
 				<div class="left-content">
 					<h5>motivis learning</h5>
-					<section>
+					<article>
 						<h4><a href='#'>From the Field: Clearing the Path for CBE Within the UNC System</a></h4>
 						<small>6/20/2016 | Emily Dustin</small>
-					</section>
-					<section>
+					</article>
+					<article>
 						<h4><a href='#'>Artificial Roadblocks to Competency-Based Education</a></h4>
 						<small>6/20/2016 | AJ Dellicicchi</small>
-					</section>
+					</article>
 				</div>
 			</div>	
 			<div class="col-sm-12 footer-right">
 				<div class="right-content">
 					<h5>motivis platform</h5>
 					<ul>
-						<li><h4><a href="#">learning relationship management</a></h4></li>
-						<li><h4><a href="#">networking & school community</a></h4></li>
-						<li><h4><a href="#">student information systems</a></h4></li>
-						<li><a href="/demo.php" class="btn">Request Demo</a></li>
+						<li><a href="#">learning relationship management</a></li>
+						<li><a href="#">networking &amp; school community</a></li>
+						<li><a href="#">student information systems</a></li>
 					</ul>
+					<a href="/demo.php" class="btn">Request Demo</a>
 				</div>
 			</div>
 		</div>
