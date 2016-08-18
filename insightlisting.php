afce51a5f8b5b526f52b097e4ff2a4f6f3a49173
Greg Saras <Greg@GDogg.local>
2016-08-18 17:28:48 -0400

    Added styling for insights listing. Added insights listing page, the HTML was just ripped from what is displayed on WP. Fixed issue where centering wasn't working due to override.

---
+++ insightlisting.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+        <title>Motivis</title>
+        <meta name="description" content="">
+        <?php include 'includes/head.php'; ?>
+        </head>
+    <body class="insightslisting">
+        <?php include 'includes/header.php'; ?>
+        <main id="main" class="site-main" role="main">
+			<article id="post-3878" class="post-3878 post type-post status-publish format-standard hentry category-education category-learning tag-assessment tag-cbe tag-competency-based-education tag-competency-based-learning">
+			<a class="category" href="http://motiviswp.thisbetterworkbetter.com/category/education/">Education</a>
+					| August 12, 2016
+				<h3 class="entry-title">
+					<a href="http://motiviswp.thisbetterworkbetter.com/2016/08/12/competency-based-learning-assessment/" rel="bookmark">Competency-Based Learning &amp; Assessment</a>
+				</h3>					
+			</article><!-- #post-## -->
+
+			<article id="post-1" class="post-1 post type-post status-publish format-standard hentry category-case-study tag-cbe tag-competency-based-education tag-competency-based-learning tag-higher-ed tag-student-centered-learning">
+				<a href="http://motiviswp.thisbetterworkbetter.com/category/case-study/">Case Study</a>
+					| June 20, 2016
+				<h3 class="entry-title">
+					<a href="http://motiviswp.thisbetterworkbetter.com/2016/06/20/from-the-field-clearing-the-path-for-cbe-within-the-unc-systy/" rel="bookmark">From the Field: Clearing the Path for CBE Within the UNC Systy</a>
+				</h3>					
+				<p>Dr. Solér provides strategic assistance, communication and program development support for student competencies. She aligns assessment efforts in the Division of Academic Affairs with the Office of Learning Technology and Innovation initiatives to help ensure campus and student success. Dr. Solér works with faculty and staff across the university system on collaborative competency-based programming and assessment projects.</p>
+			</article><!-- #post-## -->
+			<nav class="navigation pagination" role="navigation">
+				<h2 class="screen-reader-text">Posts navigation</h2>
+				<div class="nav-links">
+					<span class="page-numbers current">
+						<span class="meta-nav screen-reader-text">Page </span>1
+					</span>
+					<a class="page-numbers" href="http://motiviswp.thisbetterworkbetter.com/insights/page/2/"><span class="meta-nav screen-reader-text">Page </span>2</a>
+					<span class="page-numbers dots">…</span>
+					<a class="page-numbers" href="http://motiviswp.thisbetterworkbetter.com/insights/page/8/"><span class="meta-nav screen-reader-text">Page </span>8</a>
+					<a class="next page-numbers" href="http://motiviswp.thisbetterworkbetter.com/insights/page/2/">Next page</a>
+				</div>
+			</nav>			
+		</main>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
