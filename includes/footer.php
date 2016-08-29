e07303e359594bde75f9c8733facf8df89bd4652
Greg Saras <Greg@GDogg.local>
2016-08-29 11:04:14 -0400

    Sorry but we have a new footer, structure should be all set now...hopefully

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,60 @@
 <footer>
-	<section class="upper">
-		<div class="row">
-			<div class="col-sm-12 footer-left matchHeight">
-				<div class="left-content">
-					<ul class="sub-menu">
-                       	<li><h5 class="allCaps">motivis platform</h5></li>
-                       	<li><a href="#">Lerning Relationship Management</a></li>
-                       	<li><a href="#">Social Networking & School Community</a></li>
-                       	<li><a href="#">Career Pathing for Students & Employers</a></li>
-                       	<li><a href="#">Student Information Systems</a></li>
-                       	<li><a href="#">Student Admissions</a></li>
-                       	<li><a href="#">Alumni Engagement</a></li>
-                    </ul>
-                    <a href="/demo.php" class="btn">Request Demo</a>
+	<section class="background">
+		<div class="inner">
+			<div class="row">
+				<div class="col-sm-12 matchHeight footer-elm">
+					<div class="padding">
+						<ul class="sub-menu">
+			               	<li><h5 class="allCaps">motivis platform</h5></li>
+			               	<li><a href="#">Lerning Relationship Management</a></li>
+			               	<li><a href="#">Social Networking & School Community</a></li>
+			               	<li><a href="#">Career Pathing for Students & Employers</a></li>
+			               	<li><a href="#">Student Information Systems</a></li>
+			               	<li><a href="#">Student Admissions</a></li>
+			               	<li><a href="#">Alumni Engagement</a></li>
+			            </ul>
+			            <a href="/demo.php" class="btn">Request Demo</a>
+		            </div>
 				</div>
-			</div>
-			<div class="col-sm-12 footer-right matchHeight">
-				<div class="right-content">
-					<small>motivis learning</small>
-					<h4><a href='#'>From the Field: Clearing the Path for CBE Within the UNC System</a></h4>
-					<small>6/20/2016 | Emily Dustin</small>
-					<h4><a href='#'>Artificial Roadblocks to Competency-Based Education</a></h4>
-					<small>6/20/2016 | AJ Dellicicchi</small>
+				<div class="col-sm-12 matchHeight footer-elm">	
+					<div class="padding">		
+						<small>insights</small>
+						<h4><a href='#'>From the Field: Clearing the Path for CBE Within the UNC System</a></h4>
+						<small>6/20/2016 | Emily Dustin</small>
+						<h4><a href='#'>Artificial Roadblocks to Competency-Based Education</a></h4>
+						<small>6/20/2016 | AJ Dellicicchi</small>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-	<section class="lower">
-		<div class="inner">
-			<a href="/" class="logo">Motivis Learning</a>
-			<div class="row">
-				<div class="col-md-16 col-sm-12 contact-info">
-					<div class="row">
-						<div class="col-md-12 address">
-							<p>
-								<a href="https://www.google.com/maps/place/25+Pelham+Rd,+Salem,+NH+03079/@42.7756873,-71.2473937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e3abceed38b001:0x8f7069972017030b!8m2!3d42.7756873!4d-71.2473937">Copyright 2016 Motivis Learning Systems<br />25 Pelham Road, Unit 204<br />Salem, NH 03079</a>
-							</p>
-						</div>
-						<div class="col-md-12">
-							<p>
-								<a href="tel:+1855-202-6500">855-202-6500</a>
-								<br />
-								<a href="mailto:info@motivislearning.com">info@motivislearning.com</a>
-							</p>
-						</div>
+	<div class="inner">
+		<a href="/" class="logo">Motivis Learning</a>
+		<div class="row">
+			<div class="col-md-16 col-sm-12 contact-info">
+				<div class="row">
+					<div class="col-md-12 address">
+						<p>
+							Copyright 2016 Motivis Learning Systems<br />
+							<a href="https://www.google.com/maps/place/25+Pelham+Rd,+Salem,+NH+03079/@42.7756873,-71.2473937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e3abceed38b001:0x8f7069972017030b!8m2!3d42.7756873!4d-71.2473937">25 Pelham Road, Unit 204<br />Salem, NH 03079</a>
+						</p>
+					</div>
+					<div class="col-md-12">
+						<p>
+							<a href="tel:+1855-202-6500">855-202-6500</a>
+							<br />
+							<a href="mailto:info@motivislearning.com">info@motivislearning.com</a>
+						</p>
 					</div>
 				</div>
-				<div class="col-md-8 col-sm-12 social-icon-container">
-					<a href="#"><i class="motivis motivis-twitter"></i></a>
-					<a href="#"><i class="motivis motivis-linkedin"></i></a>
-					<a href="#"><i class="motivis motivis-facebook"></i></a>
-				</div>
 			</div>
-	</section>
+			<div class="col-md-8 col-sm-12 social-icon-container">
+				<a href="#"><i class="motivis motivis-twitter"></i></a>
+				<a href="#"><i class="motivis motivis-linkedin"></i></a>
+				<a href="#"><i class="motivis motivis-facebook"></i></a>
+			</div>
+		</div>
+	</div>
 </footer>
 <script src="js/bootstrap.js"></script>
 <script src="js/plugins.js"></script>
