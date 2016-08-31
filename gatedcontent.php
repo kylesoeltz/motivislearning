bec3e60e849e564efbd35d79528de411a35ab7c5
Greg Saras <Greg@GDogg.local>
2016-08-31 10:37:50 -0400

    Added bootstrap offsets for padding to remove conflicts

--- gatedcontent.php
+++ gatedcontent.php
@@ -23,50 +23,33 @@
 		        		<h1>Community: How to Help College Students Succeed</h1>
 	        		</div>
 	        	</header>
-	        	<section class="white">
-	        		<div class="inner">
-	        			<div class="row">
-	        				<div class="col-md-4 matchHeight">
-	        					<img src="images/gated-content.jpg" />
-	        				</div>
-	        				<div class="col-md-12 matchHeight">
-	        					<p>There's a lot of buzz about creating a connected campus. But why, really, is it so important to build student community on campus? And how do you actually go about doing it?</p>
-	        					<p>This eBook explains why community, relationships and the resulting sense of mattering can make all the difference for three specific student populations:</p>
-	        					<ul>
-	        						<li>Latino/Latina Students</li>
-	        						<li>First-Generation Students</li>
-	        						<li>Online Learners</li>
-	        					</ul>
-	        				</div>
-	        				<div class="col-md-8 matchHeight">
-	        					<script>
-									hbspt.forms.create({ 
-								    	css: '',
-								    	portalId: '1625919',
-								    	formId: '927622a7-6092-4234-b59f-ae052b53a778'
-								  	});
-								</script>
-	        				</div>
-	        			</div>
-	        		</div>
-	        	</section>
-	        	<section class="solid-banner navy text-center">
-		        	<div class="inner">
-		        		<div class="section">
-			        		<h4>You'll also get 7 tips on how to enable and encourage student community for these populations.</h4>
-		        		</div>
-	        		</div>
-	        	</section>
-	        	<section class="case-study content-right white">
-		        	<div class="image-half" style="background-image: url('images/harvard-bg.jpg');">
-		        	</div>
-		        	<div class="inner">
-			        	<div class="col-sm-11">
-			        		<p>There's a lot of buzz about creating a connected campus. But why, really, is it so important to build student community on campus? And how do you actually go about doing it?</p>
-			        		<p>There's a lot of buzz about creating a connected campus. But why, really, is it so important to build student community on campus? And how do you actually go about doing it?</p>
-			        	</div>
-		        	</div>
-	        	</section>
+	        	              <section class="white">
+                    <div class="inner">
+                        <div class="row">
+                            <div class="col-md-4 matchHeight">
+                                <img src="images/gated-content.jpg" />
+                            </div>
+                            <div class="col-md-10 col-md-offset-1 matchHeight">
+                                <p>There's a lot of buzz about creating a connected campus. But why, really, is it so important to build student community on campus? And how do you actually go about doing it?</p>
+                                <p>This eBook explains why community, relationships and the resulting sense of mattering can make all the difference for three specific student populations:</p>
+                                <ul>
+                                    <li>Latino/Latina Students</li>
+                                    <li>First-Generation Students</li>
+                                    <li>Online Learners</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-8 col-md-offset-1 matchHeight">
+                                <script>
+                                    hbspt.forms.create({ 
+                                        css: '',
+                                        portalId: '1625919',
+                                        formId: '927622a7-6092-4234-b59f-ae052b53a778'
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 		    </article>
     	</main>
     	<?php include 'includes/footer.php'; ?> 		
